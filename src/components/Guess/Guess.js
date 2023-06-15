41a2fbb4efeeba38ffd6c3c84dd68f2754ae5662
Agnieszka Gorz <agnieszka.gorz@nopio.com>
2023-06-15 21:10:04 +0200

    excersise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers.js';
 
-function Guess({ guess }) {
+// In the result Josh is using the separate component Cell 
+// https://github.com/joy-of-react/word-clone/commit/7afdb36ca5a77544b14d61ea347b094352e47818
+
+function Guess({ guess, answer }) {
+  const checkedGuess = checkGuess(guess, answer);
   return (
     <p className='guess'>
       {range(5).map((index) => {
         return (
-          <span key={index} className='cell'>
-            {guess != undefined && [...guess][index]}
+          <span
+            key={index}
+            className={
+              guess !== undefined ? `cell ${checkedGuess[index].status}` : 'cell'
+            }
+          >
+            {guess !== undefined && [...guess][index]}
           </span>
         );
       })}
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -4,11 +4,11 @@ import Guess from '../Guess';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { range } from '../../utils';
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className='guess-results'>
       {range(NUM_OF_GUESSES_ALLOWED).map((index) => {
-        return <Guess key={index} guess={guesses[index]} />;
+        return <Guess key={index} answer={answer} guess={guesses[index]} />;
       })}
     </div>
   );
